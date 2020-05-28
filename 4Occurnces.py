0599f5046318437a5533a4f0f2c851e97aa26b97
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-05-28 09:53:19 +0530

    Add files via upload

---
+++ 4Occurnces.py
@@ -0,0 +1,14 @@
+#Occurrences of ‘4’
+
+T=int(input())
+for i in range(T):
+    x=input()
+    count=0
+    for i in x:
+       if i=='4':
+           count+=1
+    print(count)
+def problem:
+     //code
+If __name__ == “__main__”
+    Print(problem())
