fa1ae8acf3372c29d99dc50d05261d85c48b1904
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-06-01 22:36:41 +0530

    Update BiggestForset.py

--- BiggestForset.py
+++ BiggestForset.py
@@ -69,3 +69,54 @@ if(len(k)==0):
     print(0)
 else:
     print(max(k))
+
+    
+    
+ void track(string plane[], int n, int x, int y, int &cur, int &max );
+ 
+int main(){
+    int n;
+    cin>>n;
+    string plane[n];
+    cin.get();
+    for (int i=0;i<n;++i) getline(cin,plane[i],'\n');
+    int cur=0, max=0;
+    for (int i=0;i<n;++i)
+        for (int j=0;j<n;++j){
+            if (plane[i][j]=='T')
+                            cur=0;
+                track(plane,n,i,j,cur,max);
+        }
+    cout<<max;
+    return 0;
+}
+ 
+void track(string plane[], int n, int x , int y, int &cur, int &max){
+    if (plane[x][y]=='W'){
+        if (cur>max) max= cur;
+        return ;
+    }
+    if (plane[x][y]=='T'){
+        plane[x][y]='W';
+        cur++;
+        if (x+1<n) track(plane,n,x+1,y,cur,max);
+        if (y+1<n) track(plane,n,x,y+1,cur,max);
+        if (x-1>=0) track(plane,n,x-1,y,cur,max);
+        if (y-1>=0) track(plane,n,x,y-1,cur,max);
+    }
+}
+
+
+
+def calculateMax(forest,i,j,n):
+    if(i<0 or j<0 or i>=n or j>=n):
+        return 0
+    count=0
+    if(forest[i][j]=='T'):
+        count+=1
+        forest[i][j]='W'
+        count+=calculateMax(forest,i+1,j,n)
+        count+=calculateMax(forest,i-1,j,n)
+        count+=calculateMax(forest,i,j+1,n)
+        count+=calculateMax(forest,i,j-1,n)
+    return count
