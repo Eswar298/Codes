6c300d25026a1fc71b1416e4f4fb25c358b3379e
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-06-03 09:13:56 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,47 @@
+-->n = int(input())
+-->x,y = map(int , input().split())
+-->len(li)  length of list
+-->Reverse the string
+   s[::-1]
+   
+-->list sort
+li.sort()
+li.sort(reverse = true)
+
+-->string sort
+sorted(str)
+
+-->li.append(ele)
+-->li.insert(index,ele)
+
+-->math.factorial(ele)
+-->math.sqrt(ele)
+-->math.gcd(x,y)
+
+-->pow(x,y)
+
+-->print(gcd,lcm,end=' ')
+
+-->sum(list)
+-->max(list)
+
+-->if 'i' in string:
+        print("found")
+      
+-->s = input()
+intF = int(s[0])
+intL = int(s[-1])
+
+-->str.count('i') > 0
+
+-->str="hello"
+   substr="hell"
+   str.count(substr)   --> gives count
+   
+-->format
+{0:0.6}.format(a*b*0.9)
+
+0 means first element
+0.6 -->6 decimals after result
+
+-->li = list(map(int,input().split()))
