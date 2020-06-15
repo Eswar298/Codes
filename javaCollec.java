5539abcf2d5e46451f6336d7c7d9be074ae8c55c
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-06-15 10:24:30 +0530

    Create javaCollec.java

---
+++ javaCollec.java
@@ -0,0 +1,166 @@
+-->The Collection in Java is a framework that provides an architecture to store and manipulate the group of objects.
+
+-->The Collection framework represents a unified architecture for storing and manipulating a group of objects. It has:
+
+1.Interfaces and its implementations, i.e., classes
+2.Algorithm
+
+import java.util.*;  
+class TestJavaCollection1{  
+public static void main(String args[]){  
+ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+list.add("Ravi");//Adding object in arraylist  
+list.add("Vijay");  
+list.add("Ravi");  
+list.add("Ajay");  
+//Traversing list through Iterator  
+Iterator itr=list.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
+
+
+import java.util.*;  
+public class TestJavaCollection2{  
+public static void main(String args[]){  
+LinkedList<String> al=new LinkedList<String>();  
+al.add("Ravi");  
+al.add("Vijay");  
+al.add("Ravi");  
+al.add("Ajay");  
+Iterator<String> itr=al.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
+
+
+import java.util.*;  
+public class TestJavaCollection3{  
+public static void main(String args[]){  
+Vector<String> v=new Vector<String>();  
+v.add("Ayush");  
+v.add("Amit");  
+v.add("Ashish");  
+v.add("Garima");  
+Iterator<String> itr=v.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+} 
+
+
+import java.util.*;  
+public class TestJavaCollection4{  
+public static void main(String args[]){  
+Stack<String> stack = new Stack<String>();  
+stack.push("Ayush");  
+stack.push("Garvit");  
+stack.push("Amit");  
+stack.push("Ashish");  
+stack.push("Garima");  
+stack.pop();  
+Iterator<String> itr=stack.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}
+
+
+import java.util.*;  
+public class TestJavaCollection5{  
+public static void main(String args[]){  
+PriorityQueue<String> queue=new PriorityQueue<String>();  
+queue.add("Amit Sharma");  
+queue.add("Vijay Raj");  
+queue.add("JaiShankar");  
+queue.add("Raj");  
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr=queue.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+queue.remove();  
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr2=queue.iterator();  
+while(itr2.hasNext()){  
+System.out.println(itr2.next());  
+}  
+}  
+}  
+
+
+
+import java.util.*;  
+public class TestJavaCollection6{  
+public static void main(String[] args) {  
+//Creating Deque and adding elements  
+Deque<String> deque = new ArrayDeque<String>();  
+deque.add("Gautam");  
+deque.add("Karan");  
+deque.add("Ajay");  
+//Traversing elements  
+for (String str : deque) {  
+System.out.println(str);  
+}  
+}  
+}  
+
+
+import java.util.*;  
+public class TestJavaCollection7{  
+public static void main(String args[]){  
+//Creating HashSet and adding elements  
+HashSet<String> set=new HashSet<String>();  
+set.add("Ravi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");  
+//Traversing elements  
+Iterator<String> itr=set.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
+
+
+import java.util.*;  
+public class TestJavaCollection8{  
+public static void main(String args[]){  
+LinkedHashSet<String> set=new LinkedHashSet<String>();  
+set.add("Ravi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");  
+Iterator<String> itr=set.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
+
+import java.util.*;  
+public class TestJavaCollection9{  
+public static void main(String args[]){  
+//Creating and adding elements  
+TreeSet<String> set=new TreeSet<String>();  
+set.add("Ravi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");  
+//traversing elements  
+Iterator<String> itr=set.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
