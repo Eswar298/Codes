a95f91baaefed4a9ececbe25d2f7dbed02a98864
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-06-15 13:12:47 +0530

    Update ATMjava.java

--- ATMjava.java
+++ ATMjava.java
@@ -106,3 +106,118 @@ class Codechef
 			t--;}
 	}
 }
+
+Lapindrome
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while(n-- > 0) {
+		    String s = sc.next();
+		    if(s.length() % 2 == 0) {
+		        int[] arr = new int[26];
+		        for(int i = 0; i < s.length() / 2; i++) {
+		            arr[s.charAt(i) - 'a']++;
+		        }
+		        for(int i = s.length() / 2; i < s.length(); i++) {
+		            arr[s.charAt(i) - 'a']--;
+		        }
+		        boolean b = true;
+		        for(int i = 0; i < arr.length; i++) {
+		            if(arr[i] != 0) {b = false;break;}
+		        }
+		        System.out.println(((b) ? "YES": "NO"));
+		    } else {
+		        int[] arr = new int[26];
+		        for(int i = 0; i < s.length() / 2; i++) {
+		            arr[s.charAt(i) - 'a']++;
+		        }
+		        for(int i = s.length() / 2 + 1; i < s.length(); i++) {
+		            arr[s.charAt(i) - 'a']--;
+		        }
+		        boolean b = true;
+		        for(int i = 0; i < arr.length; i++) {
+		            if(arr[i] != 0) {b = false;break;}
+		        }
+		        System.out.println(((b) ? "YES": "NO"));
+		    }
+		}
+	}
+}
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- != 0) {
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    int k = sc.nextInt();
+		    int x = Math.abs(n - m);
+		    if (k < x) {
+		        System.out.println(x - k);
+		    } else {
+		        System.out.println("0");
+		    }
+		}
+	}
+}
+
+
+ public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String a[]=s.split(" ");
+        int t=Integer.parseInt(a[0]);
+        int n=Integer.parseInt(a[1]);
+        System.out.println(t+" "+n);
+    }
+
+
+public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		sc.nextLine();
+		List<String> arr = new ArrayList<String>();
+		
+		while(k-->0){
+		    
+		    String str1 = sc.nextLine();
+		    
+		    if(str1.equals("CLOSEALL")){
+		        arr.clear();
+		        System.out.println(arr.size());
+		    }
+		    else if(arr.contains(str1)){
+		            arr.remove(str1);
+		            System.out.println(arr.size());
+		    }
+		    else{
+		        arr.add(str1);
+		        System.out.println(arr.size());
+		    }
+		    
+		}
