f1a9f43f859a0d7b4fe9b8c5bc9a58bc870fa826
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-06-21 17:12:29 +0530

    Create BetweenTwosets.cpp

---
+++ BetweenTwosets.cpp
@@ -0,0 +1,15 @@
+https://www.hackerrank.com/challenges/between-two-sets/problem?h_r=profile&isFullScreen=true
+
+int cnt = 0;
+    for (int k=1; k<=100; k++)
+    {
+        int flag = 1;
+        for (int i=0; i<n; i++)
+            if (k % a[i] != 0)
+                flag = 0;
+        for (int i=0; i<m; i++)
+            if (b[i] % k != 0)
+                flag = 0;
+        if (flag == 1)
+            cnt ++;
+    }
