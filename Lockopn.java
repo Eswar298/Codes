ccac2a0a58863ecd4e28b363067d3f7d16b9c3f7
Eswar298 <66050321+Eswar298@users.noreply.github.com>
2020-06-24 19:46:09 +0530

    Create Lockopn.java

---
+++ Lockopn.java
@@ -0,0 +1,39 @@
+https://leetcode.com/problems/open-the-lock/
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int openLock(String[] deadends, String target) {
+        Set<String> dead = new HashSet<>();
+        for (String s : deadends) dead.add(s);
+        if (dead.contains("0000")) return -1;
+        if ("0000".equals(target)) return 0;
+        Set<String> v = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.add("0000");
+        for (int d = 1; !q.isEmpty(); d++) {
+            for (int n = q.size(); n > 0; n--) {
+                String cur = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    for (int dif = 1; dif <= 9; dif += 8) {
+                        char[] ca = cur.toCharArray();
+                        ca[i] = (char)((ca[i] - '0' + dif) % 10 + '0');
+                        String s = new String(ca);
+                        System.out.println("string is"+s);
+                        if (target.equals(s)) return d;
+                        if (!dead.contains(s) && !v.contains(s)) q.add(s);
+                        v.add(s);
+                    }
+                }
+            }
+            System.out.println("d changed");
+        }
+        return -1;        
+    }
+
+    public static void main(String args[]) {
+        String deadends[]={"0201","0101","0102","1212","2002"};
+        String target="0202";
+        System.out.println(openLock(deadends,target));
+    }
+}
